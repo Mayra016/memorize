8d032e46c3d762d3d22515115b1ec6b51de6d97a
Mayra016 <mayrazucaro@gmail.com>
2025-02-07 09:04:47 -0300

    feat add translation to index.html

---
+++ config/translationEN.js
@@ -0,0 +1,7 @@
+const textEN = {
+    titleIndex : 'How to play?',
+    helpText : 'Pay attetion at the sequence of lights and submit the sequence to complete the levels',
+    play : 'Play'
+}
+
+export default textEN;
---
+++ config/translationES.js
@@ -0,0 +1,7 @@
+const textES = {
+    titleIndex : '¿Cómo jugar?',
+    helpText : 'Memoriza la secuencia de colores e insertalos para completar los niveles',
+    play : 'Jugar'
+}
+
+export default textES;
